cfc438a1c314fafebdea3a13cd921aace31316fb
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2020-12-11 20:01:29 -0300

    Aplicação - simulação de balanceamento de carga na intancia aws

---
+++ bootcamp/aplicacao/s3-conf.php
@@ -0,0 +1,8 @@
+<?php
+$accessKey = 'AKIAZ4KLAPOXI7XLHHOJ';
+$secretKey = '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';
+$region = 'us-east-1';
+$bucket = 'bootcamp2021.cloud';
+$arqName =  'logo.jpg';
+$linkestatico = 'http://bootcamp2021.cloud.s3-website-us-east-1.amazonaws.com'
+?>
